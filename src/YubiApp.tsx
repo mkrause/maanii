684f05f65db4173b24ce0d9bfcf4fbc7c0923328
mkrause <maikel.krause@fortanix.com>
2023-01-20 06:57:09 +0700

    WIP

--- src/YubiApp.tsx
+++ src/YubiApp.tsx
@@ -6,18 +6,19 @@ export const YubiApp = () => {
   return (
     <main>
       <div className="scroll-container">
-        <div className="scroll-area">มานี</div>
         <div className="scroll-area"></div>
         <div className="scroll-area"></div>
         <div className="scroll-area"></div>
         <div className="scroll-area"></div>
         <div className="scroll-area"></div>
         <div className="scroll-area"></div>
-        <div className="scroll-area"></div>
-        <div className="scroll-area"></div>
-        <div className="scroll-area"></div>
-        <div className="scroll-area"></div>
-        <div className="scroll-area"></div>
+        <div className="scroll-area">
+          <div className="cover1"></div>
+          <p>
+            คุณ จะ แต่งงาน<br/>
+            กับ ผม ไหม
+          </p>
+        </div>
       </div>
     </main>
   );

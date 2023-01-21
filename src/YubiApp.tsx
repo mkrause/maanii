e50734bc64146ba153ca255d761f6e4ffd7d97b3
mkrause <maikel.krause@fortanix.com>
2023-01-22 01:49:50 +0700

    Fix mobile + add triggers.

--- src/YubiApp.tsx
+++ src/YubiApp.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 
 
 export const YubiApp = () => {
+  const [count, setCount] = React.useState(0);
   return (
     <main>
       <div className="scroll-container">
@@ -12,11 +13,22 @@ export const YubiApp = () => {
         <div className="scroll-area"></div>
         <div className="scroll-area"></div>
         <div className="scroll-area"></div>
-        <div className="scroll-area">
+        <div className="scroll-area yubi">
           <div className="cover1"></div>
+          <div className="trigger1" onClick={() => { setCount(count => Math.max(count, 1)); }}></div>
+          <div className="trigger2" onClick={() => { setCount(count => count === 0 ? 0 : Math.max(count, 2)); }}></div>
           <p>
-            คุณ จะ แต่งงาน<br/>
-            กับ ผม ไหม
+            ชูใจ ถือ อะไร<br/>
+            ชูใจ ถือ แหวน ใน มือ<br/>
+            <span className={count >= 1 ? 'visible' : 'hidden'}>
+              <br/>
+              เทียน<br/>
+            </span>
+            <span className={count >= 2 ? 'visible' : 'hidden'}>
+              <br/>
+              คุณ จะ แต่งงาน<br/>
+              กับ ผม ไหม
+            </span>
           </p>
         </div>
       </div>

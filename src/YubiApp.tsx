1810d6f5e49b5c84b40f1e35179317e29d66d057
mkrause <maikel.krause@fortanix.com>
2023-01-22 22:49:58 +0700

    Update animation

--- src/YubiApp.tsx
+++ src/YubiApp.tsx
@@ -4,9 +4,18 @@ import * as React from 'react';
 
 export const YubiApp = () => {
   const [count, setCount] = React.useState(0);
+  const name = count >= 2 ? 'ไมเคิล' : 'ชูใจ';
+  
+  React.useEffect(() => {
+    if (count === 2) {
+      window.setTimeout(() => { setCount(3); }, 8000);
+    }
+  }, [count]);
+  
   return (
     <main>
       <div className="scroll-container">
+        <div className="scroll-area"></div>
         <div className="scroll-area"></div>
         <div className="scroll-area"></div>
         <div className="scroll-area"></div>
@@ -18,17 +27,28 @@ export const YubiApp = () => {
           <div className="trigger1" onClick={() => { setCount(count => Math.max(count, 1)); }}></div>
           <div className="trigger2" onClick={() => { setCount(count => count === 0 ? 0 : Math.max(count, 2)); }}></div>
           <p>
-            ชูใจ ถือ อะไร<br/>
-            ชูใจ ถือ แหวน ใน มือ<br/>
-            <span className={count >= 1 ? 'visible' : 'hidden'}>
-              <br/>
-              เทียน<br/>
+            {name} ถือ อะไร<br/>
+            {name} ถือ
+            {' '}
+            <span className={`question-marks ${count >= 1 ? 'content-visible' : 'content-hidden'}`}>
+              <span className="placeholder">???</span>
+              <span className="content">แหวน</span>
             </span>
+            {' '}
+            ใน มือ<br/>
             <span className={count >= 2 ? 'visible' : 'hidden'}>
+              <br/>
+              ไมเคิล ขอ เทียน<br/>
+              {count === 2 && '...'}
+            </span>
+            <span className={count >= 3 ? 'visible' : 'hidden'}>
+              แต่งงาน กับ เขา<br/>
+            </span>
+            {/*<span className={count >= 2 ? 'visible' : 'hidden'}>
               <br/>
               คุณ จะ แต่งงาน<br/>
               กับ ผม ไหม
-            </span>
+            </span>*/}
           </p>
         </div>
       </div>
